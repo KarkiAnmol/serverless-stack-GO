ff28dcfea29b9c3e0509de0715b6ae551fae2660
ciper <anmolkarki101@gmail.com>
2023-11-20 09:17:16 +0545

    feat: Implement DeleteUser function for removing a user
    In this commit, I've added the DeleteUser function to the 'user' package. This function processes an API Gateway Proxy Request, extracts the email parameter from the query string, constructs a DynamoDB DeleteItemInput to remove the user based on the email, and performs a DynamoDB DeleteItem operation. Proper error handling is implemented to handle potential issues during the user deletion process.

--- pkg/users/user.go
+++ pkg/users/user.go
@@ -142,5 +142,18 @@ func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient 
 	return &u, nil
 }
 func DeleteUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) error {
-
+	email := req.QueryStringParameters["email"]
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+	_, err := dynaClient.DeleteItem(input)
+	if err != nil {
+		return errors.New(ErrorCouldNotDeleteItem)
+	}
+	return nil
 }
